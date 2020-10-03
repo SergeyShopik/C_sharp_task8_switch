333e005721e6408b4fe4eea3449defcafd70c5ae
Sergey Shopik <sergey.shopik101@gmail.com>
2020-10-03 16:26:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace C_sharp_8_switch_branching
+{
+    class Program
+    {
+        public static void MoodSensor()
+        {
+            var rand = new Random();
+            int choice = rand.Next() % 2;
+            choice = 0;
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("    ###########\n" +
+                                      "  ##           ##\n" +
+                                      " #    ##   ##    #\n" +
+                                      "#     ##   ##     #\n" +
+                                      "#  #           #  #\n" +
+                                      " #   #       #   #\n" +
+                                      "  #    ## ##    #\n" +
+                                      "   ##         ##\n" +
+                                      "     #########\n");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("0. Exit\n1. Mood Sensor\n");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 0: flag = false; break;
+                    case 1:
+                        MoodSensor();
+                        break;
+                }
+            }
+        }
+    }
+}
